c9b2b874a065caa6b34ecb3d05ae515345c6ff57
Cloudswept <developer@cloudswept.com>
2020-06-10 23:30:37 +1200

    Initial

---
+++ esnext/index.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    json: import("@opennetwork/rdf-data-model").NamedNode<"https://www.iana.org/assignments/media-types/application/json">;
+    html: import("@opennetwork/rdf-data-model").NamedNode<"https://www.iana.org/assignments/media-types/text/html">;
+    svg: import("@opennetwork/rdf-data-model").NamedNode<"https://www.w3.org/TR/SVG/mimereg.html">;
+    png: import("@opennetwork/rdf-data-model").NamedNode<"https://www.iana.org/assignments/media-types/image/png">;
+};
+export default _default;
---
+++ esnext/index.js
@@ -0,0 +1,12 @@
+import { DefaultDataFactory } from "@opennetwork/rdf-data-model";
+const json = DefaultDataFactory.namedNode("https://www.iana.org/assignments/media-types/application/json");
+const html = DefaultDataFactory.namedNode("https://www.iana.org/assignments/media-types/text/html");
+const svg = DefaultDataFactory.namedNode("https://www.w3.org/TR/SVG/mimereg.html");
+const png = DefaultDataFactory.namedNode("https://www.iana.org/assignments/media-types/image/png");
+export default {
+    json,
+    html,
+    svg,
+    png
+};
+//# sourceMappingURL=index.js.map
